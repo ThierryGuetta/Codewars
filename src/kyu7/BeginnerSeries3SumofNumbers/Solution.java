8c5de1088c000c810f0a172480c99fef6a565e14
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-10 21:09:37 +0300

    add solution

--- src/kyu7/BeginnerSeries3SumofNumbers/Solution.java
+++ src/kyu7/BeginnerSeries3SumofNumbers/Solution.java
@@ -2,7 +2,7 @@ package kyu7.BeginnerSeries3SumofNumbers;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(GetSum(0, -4)); //10 //a>b a<b
+        System.out.println(GetSum(0, -4)); //-10 //a>b a<b
     }
 
     public static int GetSum(int a, int b) {
