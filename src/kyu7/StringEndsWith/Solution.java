6ce8ae3fbe8b614ab719a790945d402ef9646b5f
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-10 23:01:20 +0300

    add solution

---
+++ src/kyu7/StringEndsWith/Solution.java
@@ -0,0 +1,11 @@
+package kyu7.StringEndsWith;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static boolean solution(String str, String ending) {
+
+        return str.endsWith(ending);
+    }
+}
